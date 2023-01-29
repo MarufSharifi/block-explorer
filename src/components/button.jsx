27ada3a5b1c84a1c9a877b608233a761c69baa9e
Maruf Sharifi <maruf@truetale.io>
2023-01-29 22:31:37 +0430

    created button component

---
+++ src/components/button.jsx
@@ -0,0 +1,8 @@
+
+
+export const Button = ({children}) => {
+
+    return <div className="px-3 py-2 bg-blue-600 rounded-md inline-block text-white font-medium cursor-pointer">
+        {children}
+    </div>  
+}
